e18b2c9dfdafc56aa3fa0127dc2174ff34ad7b01
richard <richard@richards-macbook-pro.local>
2017-01-11 09:21:09 +0800

    Initial Commit

---
+++ ImageProcess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ImageProcess
+//
+//  Created by richard on 1/11/17.
+//  Copyright © 2017 richard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
